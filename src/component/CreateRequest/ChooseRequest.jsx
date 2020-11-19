1f2d56d14f4419a782493211f38648d570acb9ce
surlejonathan <surle.jonathan@gmail.com>
2020-11-19 22:57:54 +0100

    remove requestContext

--- src/component/CreateRequest/ChooseRequest.jsx
+++ src/component/CreateRequest/ChooseRequest.jsx
@@ -1,5 +1,4 @@
-import React, { useState, useContext } from "react";
-import RequestContext from "./RequestContext";
+import React, { useState } from "react";
 import "../../styles/css/ChooseRequest.css";
 
 const ChooseRequest = () => {
