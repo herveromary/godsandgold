4c47d88c83708ef1c2ace39dc2641961cd00c22c
surlejonathan <surle.jonathan@gmail.com>
2020-11-19 20:26:21 +0100

    chooseRequest

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import "./styles/css/variables.css";
+import ChooseRequest from "./component/CreateRequest/ChooseRequest";
 
 function App() {
   return (
     <div className='App'>
       <h1>GodsAndGold</h1>
+      <ChooseRequest />
     </div>
   );
 }
---
+++ src/component/CreateRequest/ChooseRequest.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import RequestContext from "./RequestContext";
+import "../../styles/css/ChooseRequest.css";
+
+const ChooseRequest = () => {
+  const [request, setrequest] = useState();
+
+  const handleOnChange = (e) => {
+    setrequest(e.target.value);
+  };
+
+  const requestContextValue = request;
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    requestContextValue;
+  };
+
+  return (
+    <div className='ChooseRequest'>
+      <h2 className='chooseRequest-title'>Create request</h2>
+      <form className='chooseRequest-form' onSubmit={handleOnSubmit}>
+        <h3 className='chooseRequest-subtitle'>2 - Choose your request</h3>
+        <textarea
+          type='text'
+          name='requestChoice'
+          id='requestChoice'
+          value={request}
+          onChange={handleOnChange}
+        />
+        <button type='submit'>Confirm</button>
+      </form>
+      <h3 className='mostCommonRequests-title'>Most common requests</h3>
+    </div>
+  );
+};
+
+export default ChooseRequest;
