cf2a60f357e066b0605585bda33560da10131e0c
Hervé Romary <h.romary@gmail.com>
2020-11-19 17:23:13 +0100

    login done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Login from '../src/component/Login';
+import { Route, Switch } from 'react-router-dom';
+import { GGContextProvider } from './Context';
 
 function App() {
   return (
     <div className='App'>
       <h1>GodsAndGold</h1>
+      <GGContextProvider>
+        <Switch>
+          <Route exact path='/' component={Login} />
+        </Switch>
+      </GGContextProvider>
     </div>
   );
 }
---
+++ src/component/Login.jsx
@@ -0,0 +1,60 @@
+import React, { useState, useContext } from 'react';
+import axios from 'axios';
+import { Redirect } from 'react-router-dom';
+import { GGContext } from '../Context';
+
+function Login() {
+  const [user, setUser] = useContext(GGContext);
+  const [redirect, setRedirect] = useState(false);
+  const [input, setInput] = useState({
+    login: '',
+    password: '',
+    email: '',
+  });
+
+  const onChange = (e) => {
+    setInput({
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const signIn = () => {
+    axios
+      .get('https://hookspendables.herokuapp.com/api/users/')
+      .then((response) => response.data)
+      .then((data) => data.filter((user) => user.login) === input.login)
+      .then((data) => setUser(data[0]))
+      .then(() => setRedirect(true));
+  };
+
+  return (
+    <div className='login_page'>
+      <h2>Login</h2>
+      <p>
+        Lockdown due to Covid - -532 prevents you to make offerings ?
+        GodsandGold is here for youuu
+      </p>
+      <div>
+        <input
+          placeholder='login'
+          type='text'
+          id='login'
+          name='login'
+          onChange={onChange}
+          value={input.login}
+          className='input'
+          required
+        />
+        <div className='form-data'>
+          <button type='button' onClick={() => signIn()}>
+            Sign In
+          </button>
+          <p>{input.userText}</p>
+          {redirect && <Redirect to='/ChooseGod' />}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
