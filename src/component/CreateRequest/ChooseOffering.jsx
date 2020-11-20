23f375c225ee769e4066047b70200c755e7b8d96
fdureux <fanny.dureux@gmail.com>
2020-11-20 10:16:06 +0100

    add scrollTop component

--- src/component/CreateRequest/ChooseOffering.jsx
+++ src/component/CreateRequest/ChooseOffering.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
 import Topbar from "../../Reusable/Topbar";
 import "../../styles/css/ChooseOffering.css";
 import DelayLink from "react-delay-link";
