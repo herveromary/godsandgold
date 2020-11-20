8eea03b8c302d9ce16d525ecfbf8731adf6d2372
FlorianPx <florian.poux@hotmail.fr>
2020-11-20 13:21:05 +0100

    Css choosePickUp RequestStatus

--- src/component/RequestSummary/RequestConfirmation.jsx
+++ src/component/RequestSummary/RequestConfirmation.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import RecapCard from "../../Reusable/RecapCard";
 import "../../styles/css/RequestConfirmation.css";
-import Topbar from "../../Reusable/Topbar";
 
 const RequestConfirmation = ({ state }) => {
   let userName = localStorage.getItem("user");
 
   return (
     <div className="confirm">
-      <Topbar />
       <h2>Confirmation</h2>
       <p className="textConfirmation">{`Hi ${userName}, your request was submitted ✅`}</p>
       <RecapCard details={state} />
