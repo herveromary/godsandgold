293183ca1d14bd5488d7a673f6c5d7f1d60c4f3d
Hervé Romary <h.romary@gmail.com>
2020-11-20 09:48:14 +0100

    dynamic text login

--- src/component/Login.jsx
+++ src/component/Login.jsx
@@ -1,13 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { Redirect } from 'react-router-dom';
 
 import '../styles/css/login.css';
 import logo from '../images/logo.webp';
+import TextTransition, { presets } from 'react-text-transition';
 
 function Login() {
   const [redirect, setRedirect] = useState(false);
-  let history = useHistory();
+  const [user, setUser] = useState('');
+  //let history = useHistory();
   /*   const [input, setInput] = useState({
     login: '',
     password: '',
@@ -19,9 +21,25 @@ function Login() {
   };
 
   const signIn = () => {
-    localStorage.setItem('user', JSON.stringify(input));
+    localStorage.setItem('user', JSON.stringify(user));
     setRedirect(true);
   };
+
+  const TEXTS = [
+    'Choose Your God',
+    'Make your request',
+    'Make an offering',
+    'wishes come true!',
+  ];
+
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(
+      () => setIndex((index) => index + 1),
+      4000 // every 3 seconds
+    );
+  }, []);
 
   return (
     <>
@@ -31,7 +49,12 @@ function Login() {
           src={logo}
           alt='homme invoquant les dieux autour du feu'
         />
-        <h2 className='login_title'>GodsandGold</h2>
+        <h2 className='login-slide-title'>
+          <TextTransition
+            text={TEXTS[index % TEXTS.length]}
+            springConfig={presets.wobbly}
+          />
+        </h2>
         <p className='login_app_description'>
           Can't make offerings because of lockdown amid Covid-546 B.C. ? We got
           you covered ! LogIn, offer, relax... Dieu vous le rendra.
