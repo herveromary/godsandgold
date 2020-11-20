2a4b3de46f57d3fb6a15754944b1267849a1f6f2
FlorianPx <florian.poux@hotmail.fr>
2020-11-20 11:12:02 +0100

    all css

--- src/component/RequestSummary/RequestConfirmation.jsx
+++ src/component/RequestSummary/RequestConfirmation.jsx
@@ -3,9 +3,12 @@ import RecapCard from "../../Reusable/RecapCard";
 import "../../styles/css/RequestConfirmation.css";
 
 const RequestConfirmation = ({ state }) => {
+  let userName = localStorage.getItem("user");
+
   return (
     <div>
       <h2>Confirmation</h2>
+      <p className="textConfirmation">{`Hi ${userName}, your request was submitted ✅`}</p>
       <RecapCard details={state} />
     </div>
   );
