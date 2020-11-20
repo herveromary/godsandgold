506dfc696293f022c89fd971e43d843cbafb2efc
fdureux <fanny.dureux@gmail.com>
2020-11-20 10:07:39 +0100

    add a press effect on button

--- src/component/CreateRequest/ChooseOffering.jsx
+++ src/component/CreateRequest/ChooseOffering.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Topbar from "../../Reusable/Topbar";
 import "../../styles/css/ChooseOffering.css";
+import DelayLink from "react-delay-link";
 
 import fruit from "../../styles/fruitss.png";
 import animal from "../../styles/sheep.png";
@@ -12,63 +13,94 @@ import coin from "../../styles/money.png";
 
 const ChooseOffering = () => {
   const [offering, setOffering] = useState("");
+  const [selectWine, setSelectWine] = useState(false);
+  const [selectAnimal, setSelectAnimal] = useState(false);
+  const [selectFruit, setSelectFruit] = useState(false);
+  const [selectHuman, setSelectHuman] = useState(false);
+  const [selectJewel, setSelectJewel] = useState(false);
+  const [selectMoney, setSelectMoney] = useState(false);
 
   return (
     <div className="offeringpage">
       <Topbar />
       <h2> What do you want to offer ? </h2>
       <div className="selectGift">
-        <Link
-          to={{
-            pathname: `/choosepickup`,
-            state: { offering: "a galleon of wine" },
-          }}
-          className="offerButton">
-          <p> Galleon of wine</p>
-          <img src={wine} alt="wine"></img>
-        </Link>
-        <Link
-          to={{
-            pathname: `/choosepickup`,
-            state: { offering: "an animal for sacrifice" },
-          }}
-          className="offerButton">
-          <p> Sacrificial Animal</p>
-          <img src={animal} alt="animal"></img>
-        </Link>
-        <Link
-          to={{
-            pathname: `/choosepickup`,
-            state: { offering: "a member of your family" },
-          }}
-          className="offerButton">
-          <p> Family Member</p>
-          <img src={family} alt="family"></img>
-        </Link>
-        <Link
-          to={{
-            pathname: `/choosepickup`,
-            state: { offering: "an appetizing basket full of fruits" },
-          }}
-          className="offerButton">
-          <p> Fruit basket</p>
-          <img src={fruit} alt="fruits"></img>
-        </Link>
-        <Link
-          to={{ pathname: `/choosepickup`, state: { offering: "money" } }}
-          className="offerButton">
-          <p> Oboli and drachma</p>
-          <img src={coin} alt="coins"></img>
-        </Link>
-        <Link
-          to={{
-            pathname: `/choosepickup`,
-            state: { offering: "precious and beautiful jewels" },
-          }}
-          className="offerButton">
-          <p> Precious jewelry</p>
-          <img src={jewels} alt="diamonds"></img>
-        </Link>
+        <div
+          onClick={() => setSelectWine(true)}
+          className={selectWine ? "offerButtonused" : "offerButton"}>
+          <DelayLink
+            delay={400}
+            to={{
+              pathname: `/choosepickup`,
+              state: { offering: "a galleon of wine" },
+            }}>
+            <p> Galleon of wine</p>
+            <img src={wine} alt="wine"></img>
+          </DelayLink>
+        </div>
+        <div
+          onClick={() => setSelectAnimal(true)}
+          className={selectAnimal ? "offerButtonused" : "offerButton"}>
+          <DelayLink
+            delay={400}
+            to={{
+              pathname: `/choosepickup`,
+              state: { offering: "an animal for sacrifice" },
+            }}>
+            <p> Sacrificial Animal</p>
+            <img src={animal} alt="animal"></img>
+          </DelayLink>
+        </div>
+        <div
+          onClick={() => setSelectHuman(true)}
+          className={selectHuman ? "offerButtonused" : "offerButton"}>
+          <DelayLink
+            delay={400}
+            to={{
+              pathname: `/choosepickup`,
+              state: { offering: "a member of your family" },
+            }}>
+            <p> Family Member</p>
+            <img src={family} alt="family"></img>
+          </DelayLink>
+        </div>
+        <div
+          onClick={() => setSelectFruit(true)}
+          className={selectFruit ? "offerButtonused" : "offerButton"}>
+          <DelayLink
+            delay={400}
+            to={{
+              pathname: `/choosepickup`,
+              state: { offering: "an appetizing baskey full of fresh fruits" },
+            }}>
+            <p> Fruit basket</p>
+            <img src={fruit} alt="fruits"></img>
+          </DelayLink>
+        </div>
+        <div
+          onClick={() => setSelectMoney(true)}
+          className={selectMoney ? "offerButtonused" : "offerButton"}>
+          <DelayLink
+            delay={400}
+            to={{ pathname: `/choosepickup`, state: { offering: "money" } }}>
+            <p> Oboli and drachma</p>
+            <img src={coin} alt="coins"></img>
+          </DelayLink>
+        </div>
+        <div
+          onClick={() => setSelectJewel(true)}
+          className={selectJewel ? "offerButtonused" : "offerButton"}>
+          <DelayLink
+            delay={400}
+            to={{
+              pathname: `/choosepickup`,
+              state: { offering: "precious and beautiful jewels" },
+            }}
+            className="offerButton">
+            <p> Precious jewelry</p>
+            <img src={jewels} alt="diamonds"></img>
+          </DelayLink>
+        </div>
       </div>
     </div>
   );
