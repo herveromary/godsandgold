edf762e7c05e5d348caae6ca434bec9fe7bf6959
J <jonathan.arnould33@gmail.com>
2020-11-20 05:16:03 +0100

    connexion choose god to chose request

--- src/component/CreateRequest/ChooseRequest.jsx
+++ src/component/CreateRequest/ChooseRequest.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "../../styles/css/ChooseRequest.css";
+import BottomNav from "../../Reusable/BottomNav";
 
 const ChooseRequest = () => {
   const [request, setrequest] = useState();
@@ -14,40 +15,41 @@ const ChooseRequest = () => {
   };
 
   return (
-    <div className='ChooseRequest'>
-      <h2 className='chooseRequest-title'>Create request</h2>
-      <form className='chooseRequest-form' onSubmit={handleOnSubmit}>
-        <h3 className='chooseRequest-subtitle'>2 - Choose your request</h3>
+    <div className="ChooseRequest">
+      <h2 className="chooseRequest-title">Create request</h2>
+      <form className="chooseRequest-form" onSubmit={handleOnSubmit}>
+        <h3 className="chooseRequest-subtitle">2 - Choose your request</h3>
         <textarea
-          type='text'
-          name='requestChoice'
-          id='requestChoice'
+          type="text"
+          name="requestChoice"
+          id="requestChoice"
           value={request}
           onChange={handleOnChange}
         />
-        <button type='submit'>Confirm</button>
+        <button type="submit">Confirm</button>
       </form>
-      <h3 className='mostCommonRequests-title'>Most common requests :</h3>
-      <div className='diagram-box'>
-        <div class='diagram-bar'>
-          <div class='diagram-1'>
-            <span class='request'></span>
-            <span class='PercentText '>90%</span>
+      <h3 className="mostCommonRequests-title">Most common requests :</h3>
+      <div className="diagram-box">
+        <div class="diagram-bar">
+          <div class="diagram-1">
+            <span class="request"></span>
+            <span class="PercentText ">90%</span>
           </div>
         </div>
-        <div class='diagram-bar'>
-          <div class='diagram-2'>
-            <span class='request'></span>
-            <span class='PercentText '>69%</span>
+        <div class="diagram-bar">
+          <div class="diagram-2">
+            <span class="request"></span>
+            <span class="PercentText ">69%</span>
           </div>
         </div>
-        <div class='diagram-bar'>
-          <div class='diagram-3'>
-            <span class='request'></span>
-            <span class='PercentText '>52%</span>
+        <div class="diagram-bar">
+          <div class="diagram-3">
+            <span class="request"></span>
+            <span class="PercentText ">52%</span>
           </div>
         </div>
       </div>
+      <BottomNav />
     </div>
   );
 };
