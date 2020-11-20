de7e0ae1ce5640d0bfe41950917a0028aac46a4c
J <jonathan.arnould33@gmail.com>
2020-11-20 10:29:39 +0100

    fix conflict merge

--- src/component/CreateRequest/ChooseGod.jsx
+++ src/component/CreateRequest/ChooseGod.jsx
@@ -7,7 +7,10 @@ import gods from "./godsArray";
 import TextTransition, { presets } from "react-text-transition";
 import axios from "axios";
 
+const userName = localStorage.getItem("user");
+
 const TEXTS = [
+  `Hello ${userName}`,
   "Choose Your God",
   "Make your request",
   "Make an offering",
@@ -39,7 +42,7 @@ const ChooseGod = () => {
   useEffect(() => {
     const intervalId = setInterval(
       () => setIndex((index) => index + 1),
-      3000 // every 3 seconds
+      1500 // every 3 seconds
     );
   }, []);
 
